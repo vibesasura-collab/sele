e668f4a35fa459b757e559edde14f14f4dc40d95
vibesasura-collab <vibesasura@gmail.com>
2026-02-26 15:58:08 +0530

    fix duplicate ChromeOptions error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -86,7 +86,7 @@ public class Main {
 
                     int cost = Integer.parseInt(number);
 
-                    if (cost <= 50) {
+                    if (cost <= 40) {
 
                         goldAttack.get(0).click();
                         Thread.sleep(1200);
